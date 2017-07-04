1a025a67acb1bacc182761aa0652dc63f0fe7987
Kishlin <legrand.pierrelouis@gmail.com>
2017-07-04 22:27:25 +0200

    Firebase and react-router setup.

---
+++ src/constants/.constants.js
@@ -0,0 +1,8 @@
+export const DATABASE = {
+    apiKey: "#KEY#",
+    authDomain: "#AUTH#.firebaseapp.com",
+    databaseURL: "https://#URL#.firebaseio.com",
+    projectId: "#ID#",
+    storageBucket: "#BUCKET#.appspot.com",
+    messagingSenderId: "#ID#"
+  };
