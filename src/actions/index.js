5565fa447efc82eb5f88e87a55c1d6ea0f7c9d88
Kishlin <legrand.pierrelouis@gmail.com>
2017-07-06 19:13:59 +0200

    POST new posts to api.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,24 @@
 import axios from 'axios';
 
-import { FETCH_POSTS, URL_POSTS_ALL } from '../constants/constants';
+import { FETCH_POSTS, CREATE_POST, URL_POSTS } from '../constants/constants';
 
 export function fetchPosts() {
-	const request = axios.get(URL_POSTS_ALL);
+	const request = axios.get(URL_POSTS);
 	
 	return {
 		type: FETCH_POSTS,
+		payload: request
+	};
+}
+
+export function createPost(values, callback) {
+	const request = axios
+		.post(URL_POSTS, values)
+		.then(() => callback())
+	;
+
+	return {
+		type: CREATE_POST,
 		payload: request
 	};
 }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,14 +1,17 @@
+import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
 	onSubmit(values) {
-		console.log(values);
+		this.props.createPost(values, () => this.props.history.push('/'));
 	}
 
 	renderField(field) {
 		const { meta: { touched, error } } = field;
-		const className = `form-group ${touched && error ? 'has-danger': ''}`;
+		const className = `form-group ${touched && error ? 'has-danger' : ''}`;
 
 		return (
 			<div className={className}>
@@ -47,6 +50,7 @@ class PostsNew extends Component {
 						component={this.renderField}
 					/>
 					<button type="submit" className="btn btn-primary">Submit</button>
+					<Link to="/" className="btn btn-danger">Cancel</Link>
 				</form>
 			</div>
 		);
@@ -77,4 +81,6 @@ function validate(values) {
 export default reduxForm({
 	validate,
 	form: 'PostsNewForm'
-})(PostsNew);
+})(
+	connect(null, { createPost })(PostsNew)
+);
